47623f13b57ca4e1459d1201ececfd8735bf6f10
YiZhen Choo <yizhen.c02@gmail.com>
2022-09-12 13:28:15 +0800

    mvebu: add support for Linksys WRT1900 v1
    Signed-off-by: YiZhen Choo <yizhen.c02@gmail.com>

---
+++ devices/Linksys-WRT1900_v1/diy-part2.sh
@@ -0,0 +1,15 @@
+# =================================================================== #
+#                                                                     #
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>                 #
+# Copyright (c) 2022 SolomonRicky <http://firmware.download.yzyz.ga>  #
+#                                                                     #
+# This is free software, licensed under the GNU GPLv3 License.        #
+# See /LICENSE for more information.                                  #
+#                                                                     #
+# =================================================================== #
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+wget https://raw.githubusercontent.com/solomonricky/scripts/IPQ807x/import_feeds.sh
+chmod +x import_feeds.sh
+./import_feeds.sh
